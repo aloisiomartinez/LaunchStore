49ab97c949324ceb0b145a6c43b461208782eebf
Aloisio Martinez <aloisiojacobs36@gmail.com>
2020-02-06 11:35:47 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const nunjucks = require("nunjucks");
 const routes = require("./routes")
 const methodOverride = require('method-override')
+const session = require('./config/session')
 
 const server = express();
 
+server.use(session)
 server.use(express.urlencoded({ extended: true })) //Responsável por fazer funcionar o req.body
-server.use(express.static("public")); 
+server.use(express.static("public"))
 server.use(methodOverride('_method'))
 server.use(routes)
 
@@ -16,8 +18,8 @@ nunjucks.configure("src/app/views", {
   express: server,
   autoescape: false,
   noCache: true
-});
+})
 
 server.listen(5000, function() {
   console.log("server is running");
-});
+})
